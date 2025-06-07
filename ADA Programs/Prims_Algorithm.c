51d33cf405d9e8cd3c55c4a60094ec4464732e6d
Rajath Kiran A <arajath5463@gmail.com>
2025-06-07 15:52:43 +0530

    Added Prims Program and Output image

--- ADA Programs/Prims_Algorithm.c
+++ ADA Programs/Prims_Algorithm.c
@@ -1,63 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define INFI 99
-int edges[10][10],n,wt[10][10];
+int edges[10][10], n, wt[10][10];
 void prims();
-void main()
+int main()
 {
-    int i,j;
+    int i, j;
     printf("Enter the Number of Vertices:\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter the Cost Matrix\n");
-    for(i=1;i<=n;i++)
+    for (i = 1; i <= n; i++)
     {
-        for(j=1;j<=n;j++)
+        for (j = 1; j <= n; j++)
         {
-            scanf("%d",&wt[i][j]);
+            scanf("%d", &wt[i][j]);
         }
     }
-        prims();
+    prims();
+return 0;
 }
 void prims()
 {
-  int u[10],lowcost[10],visited[10];
-  int min,mincost=0,i,j,v;
-  visited[1]=1;
-  for(i=2;i<=n;i++)
-   {
-    visited[i]=0;
-    u[i]=1;
-    lowcost[i]=wt[1][i];
-   }  
-    for(i=1;i<n-1;i++)
+    int u[10], lowcost[10], visited[10];
+    int min, mincost = 0, i, j, v;
+    visited[1] = 1;
+    for (i = 2; i <= n; i++)
     {
-     min=lowcost[2];
-     v=2;
-      for(j=3;j<=n;j++)
+        visited[i] = 0;
+        u[i] = 1;
+        lowcost[i] = wt[1][i];
+    }
+    for (i = 1; i <=n - 1; i++)
+    {
+        min = lowcost[2];
+        v = 2;
+        for (j = 3; j <= n; j++)
         {
-         if(lowcost[j]<min)
-          {
-            min=lowcost[j];
-            v=j;
-          }
+            if (lowcost[j] < min)
+            {
+                min = lowcost[j];
+                v = j;
+            }
         }
-    edges[i][1]=u[v];
-    edges[i][2]=v;
-    mincost+=lowcost[v];
-    visited[v]=1;
-    lowcost[v]=INFI;
-    for(j=2;j<=n;j++)
-    {
-        if(wt[v][j]<lowcost[j] && !visted[j])
+        edges[i][1] = u[v];
+        edges[i][2] = v;
+        mincost += lowcost[v];
+        visited[v] = 1;
+        lowcost[v] = INFI;
+
+        for (j = 2; j <= n; j++)
         {
-            lowcost[j]=wt[v][j];
-            u[v]=j;
+            if (wt[v][j] < lowcost[j] && !visited[j])
+            {
+                lowcost[j] = wt[v][j];
+                u[j] = v;
+            }
         }
     }
-    
-    printf("Edges of Minimum Spanning tree are\n");
-    printf("(%d %d)\n",edges[i][1],edges[i][2]);
-    printf("Minimum Cost is %d",mincost);
- }
-}
 
+    printf("Edges of Minimum Spanning tree are\n");
+    for (i = 1; i <=n-1; i++)
+    {
+        printf("(%d %d) ", edges[i][1], edges[i][2]);
+    }
+    printf("\nMinimum Cost is: %d", mincost);
+}
---
+++ ADA Programs/tests/kruskal_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "kruskal.h"
+
+TEST(KruskalTest, BasicTest) {
+    // Add your test cases here
+    ASSERT_EQ(1, 1);
+}
